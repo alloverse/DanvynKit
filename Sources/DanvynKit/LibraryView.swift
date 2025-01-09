f66a21f598c7d5902d13155627ec5d14f2c90977
Nevyn Bengtsson <hello@nevyn.dev>
2025-01-09 16:11:20 +0100

    LibraryView lets the user drag'n'drop assets into a scene.

---
+++ Sources/DanvynKit/LibraryView.swift
@@ -0,0 +1,79 @@
+//
+//  LibraryView.swift
+//  DanvynKit
+//
+//  Created by Nevyn Bengtsson on 2025-01-09.
+//
+
+import SwiftUI
+
+/// An item in a library that the user can drag'n'drop or tap to add to the current scene.
+public protocol LibraryItem : Equatable, Identifiable, Codable, Transferable
+{
+    /// Display name to show next to the item
+    var name: String { get }
+    /// Name of image that should be shown for the asset. Should be a string compatible with Image({}, bundle: .main))
+    var iconAsset: String  { get }
+}
+
+/// A list of items that the LibraryView contains
+public struct Library<VendedType: LibraryItem> : Equatable
+{
+    public let items: [VendedType]
+    public init(items: [VendedType])
+    {
+        self.items = items
+    }
+}
+
+/// A Library, to be presented in a Window, which allows the user to drag'n'drop items into a scene (or tap them to have them added to the middle of the scene).
+/// You probably want to subclass it and manage your contents somehow.
+public struct LibraryView<VendedType: LibraryItem> : View
+{
+    public var contents: Library<VendedType>
+    public var body: some View {
+        VStack {
+            ForEach(contents.items)
+            { template in
+                VStack {
+                    // Model3D(named: template.iconAsset) only on visionOS
+                    Image(template.iconAsset)
+                        .resizable()
+                        .frame(width: 64, height: 64)
+                    Text(template.name)
+                }
+                .padding()
+                .cornerRadius(12)
+                .overlay() {
+                    RoundedRectangle(cornerRadius: 12)
+                        .stroke(.black.opacity(0.1), lineWidth: 1)
+                }
+                
+                .frame(width: 128, height: 128)
+                .draggable(template)
+            }
+        }
+        .background(Color.black.opacity(0.3))
+    }
+}
+
+/// Convenience constructor which makes a pretty Library window suitable for the current platform.
+public func LibraryWindow<VendedType: LibraryItem>(name: String, id: String, contents: Library<VendedType>) -> some Scene
+{
+#if os(macOS)
+        Window(name, id: id) {
+            LibraryView<VendedType>(contents: contents)
+                .containerBackground(.ultraThinMaterial, for: .window)
+        }
+        .windowResizability(.contentSize)
+        .restorationBehavior(.disabled)
+        .defaultWindowPlacement { content, context in
+            return WindowPlacement(.trailing)
+        }
+#else
+        WindowGroup(id: id) {
+            LibraryView<VendedType>(contents: contents)
+                // TODO: make it nice
+        }
+#endif
+}
