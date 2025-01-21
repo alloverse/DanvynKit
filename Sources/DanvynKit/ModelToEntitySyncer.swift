ded8cd66161b2b21154a0afc9f0dccf1adaa75a4
Daniel Kennett <daniel@cascable.se>
2025-01-21 14:07:39 +0100

    Add `forceUpdates` parameter to ModeToEntitySyncer

--- Sources/DanvynKit/ModelToEntitySyncer.swift
+++ Sources/DanvynKit/ModelToEntitySyncer.swift
@@ -18,11 +18,26 @@ public actor ModelToEntitySyncer<ModelType: Equatable>
     
     public init() {}
     
+    /// Sync the given model objects with a set of RealityKit entities.
+    ///
+    /// When RealityKit entities need to be added or updated, the relevant closures will be called. When an entity
+    /// needs to be removed, it'll be removed without any callbacks.
+    ///
+    /// - Parameters:
+    ///   - newList: A dictionary of model objects keyed by each object's unique ID.
+    ///   - parent: The parent entity in which to add created model entities.
+    ///   - add: The closure to be called when a RealityKit entity needs to be created for a given model object.
+    ///          Once returned, it'll be added into the parent entity for you.
+    ///   - update: The closure to be called when a given model object has changed since the last time this method
+    ///             was called. The RealityKit entity for the model object will be provided for you to apply updates to.
+    ///   - forceUpdates: Pass `true` to always call the `update` closure for existing model objects. Useful for when
+    ///                   `Equatable` conformance isn't enough, or when using classes for model objects (the cache is noncopying).
     public func sync(
         listOfModels newList: [String: ModelType],
         asChildrenOf parent: Entity,
         add: @escaping (ModelType) async -> Entity,
-        update: @escaping (ModelType, Entity) -> Void
+        update: @escaping (ModelType, Entity) -> Void,
+        forceUpdates: Bool = false
     ) async
     {
         let toAdd = newList.filter { !cachedList.keys.contains($0.key) }
@@ -31,7 +46,7 @@ public actor ModelToEntitySyncer<ModelType: Equatable>
             let oldModel = cachedList[$0.key]
             return
                 !toRemove.keys.contains($0.key) &&
-                (oldModel == nil || oldModel! != $0.value)
+                 (oldModel == nil || (forceUpdates || oldModel! != $0.value))
         }
         
         cachedList = newList
